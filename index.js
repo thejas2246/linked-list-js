4db2e309ab82f3af3af8f7919dab3de39443010e
thejas <thejas2246@gmail.com>
2025-07-31 08:39:14 +0530

    prepend function to add node to list start

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ class LinkedList{
             temp.nextNode = node;
         }
     }
+    prepend(value){
+        if(!this.head){
+            this.head = new Node(value);
+        }
+        else{
+            let node = new Node(value);
+            node.nextNode = this.head;
+            this.head = node;
+        }
+    }
 }
 
 class Node{
@@ -24,6 +34,10 @@ class Node{
 let list = new LinkedList();
 list.append(1);
 list.append(2);
+list.prepend(8)
+list.prepend(7)
+list.prepend(5)
+
 list.append(3);
 list.append(4);
 list.append(5);
